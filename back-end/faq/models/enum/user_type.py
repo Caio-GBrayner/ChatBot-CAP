0faa30fbcda1d4e409f22f68224305dda48fdea9
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-05-14 11:09:19 -0300

    fetch(User, UserType):Model User and Enum UserType
    -Create Model User -> id, username, password..
    -Create Enum UserType -> Admin, editor, Viewer

---
+++ back-end/faq/models/enum/user_type.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class UserType(models.TextChoices):
+    ADMIN = 'AD'
+    EDITOR = 'ED'
+    VIEWER = 'VI'
---
+++ back-end/faq/models/user.py
@@ -0,0 +1,10 @@
+import uuid
+from django.db import models
+from .enum import UserType
+
+class User (models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default= uuid.uuid4())
+    username = models.CharField(max_length=255, null= False, blank=False, unique=True)
+    password = models.TextField(null= False, blank=False)
+    user_type = models.CharField(max_length=2, choices=UserType.choices, default=UserType.VIEWER)
+    created_at = models.DateTimeField(auto_now_add=True)
