0f7dff7409bbb4f72ab8e39a48a32c376d4b6b3b
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-05-20 08:32:09 -0300

    feat(User) Abstract User for model User:
    fix - Settings.py
    fix - User.py

--- back-end/faq/models/user.py
+++ back-end/faq/models/user.py
@@ -1,10 +1,16 @@
 import uuid
 from django.db import models
 from .enums import UserType
+from django.contrib.auth.models import AbstractUser
 
-class User (models.Model):
+class User (AbstractUser):
     id = models.UUIDField(primary_key=True, editable=False, default= uuid.uuid4)
-    username = models.CharField(max_length=255, null= False, blank=False, unique=True)
-    password = models.TextField(null= False, blank=False)
+    username = models.CharField(max_length=150, null= False, blank=False, unique=True)
+    first_name = None
+    last_name = None
+    email = None
     user_type = models.CharField(max_length=2, choices=UserType.choices, default=UserType.VIEWER)
-    created_at = models.DateTimeField(auto_now_add=True)
+    # created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        swappable = 'AUTH_USER_MODEL'
